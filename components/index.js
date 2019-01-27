1595d4c2de0a4afbc9b2476c0790f2c06e6845b8
Patrick Shaw <sixtey7@gmail.com>
2019-01-27 00:02:40 -0500

    work through properties and events

--- components/index.js
+++ components/index.js
@@ -20,6 +20,37 @@ Vue.component('blog-post', {
         `
 });
 
+Vue.component('trans-entry', {
+    props: {
+        name: {
+            type: String,
+            required: true
+        },
+        amount: {
+            type: Number,
+            default: 0
+        },
+        type: {
+            type: String,
+            validator: function (value) {
+                return['Planned', 'Estimate', 'Pending', 'Confirmed', 'Future'].indexOf(value) !== -1;
+            }
+        },
+        date: Date,
+        notes: String
+    },
+    template: `
+        <div class="trans-item">
+            <h3>{{ name }}</h3>
+            <p>Amount: \${{ amount }}</p>
+            <input :value="amount" type="number" @input="$emit('update:amount', parseInt($event.target.value))">
+            <p>Type: {{ type }}</p>
+            <div v-if="date"><p>Date: {{ date }}</p></div>
+            <div v-if="notes"><p>Notes: {{ notes }}</p></div>
+        </div>
+    `
+});
+
 new Vue({
     el: '#components-demo'
 });
@@ -35,3 +66,13 @@ new Vue({
         postFontSize: 1
     }
 });
+
+new Vue({
+    el: '#trans-row-demo',
+    data: {
+        transactions: [
+            { id: 1, name: 'Tx One', amount: 500, type: 'Planned', date: new Date('01/23/2018'), notes: 'I\'m a note'},
+            { id: 2, name: 'Tx Two', amount: 250, type: 'Future', notes: 'I\'m also a note'}
+        ]
+    }
+})
