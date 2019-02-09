e098b8fd555120f1095d0e554158f7e7c1697d88
Patrick Shaw <sixtey7@gmail.com>
2019-02-08 20:59:45 -0500

    continue working through render functions

--- rendering/index.js
+++ rendering/index.js
@@ -1,4 +1,39 @@
+
+var getChildrenTextContent = function (children) {
+    return children.map(function (node) {
+        return node.children ? getChildrenTextContent(node.children) : node.text
+    }).join('');
+};
+
 Vue.component('anchored-heading', {
+    render: function (createElement) {
+        //create kabab-case id
+        var headingId = getChildrenTextContent(this.$slots.default)
+            .toLowerCase()
+            .replace(/\W+/g, '-')
+            .replace(/(^-|-$)/g, '');
+
+        return createElement(
+            'h' + this.level,
+            [
+                createElement('a', {
+                    attrs: {
+                        name: headingId,
+                        href: '#' + headingId
+                    }
+                }, this.$slots.default)
+            ]
+        );
+    },
+    props: {
+        level: {
+            type: Number,
+            required: true
+        }
+    }
+});
+
+Vue.component('basic-anchored-heading', {
     render: function (createElement) {
         return createElement(
             'h' + this.level, //tag name
@@ -14,5 +49,11 @@ Vue.component('anchored-heading', {
 });
 
 new Vue({
+    el: '#basic-test-anchored-heading'
+});
+
+new Vue({
     el: '#test-anchored-heading'
 });
+
+
